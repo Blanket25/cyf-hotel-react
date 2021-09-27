886da9a73004c1ea6ec7417cfafd5f4148670b38
Blanket25 <bianca_inga@hotmail.com>
2021-09-27 13:27:58 +0200

    completed till ex.23

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,65 +1,90 @@
 import React, { useState } from "react";
 import moment from "moment";
 
-const daysBetweenDates = (a, b) => {
-  const firstMoment = moment(a);
-  const secondMoment = moment(b);
+import CustomerProfile from "./CustomerProfile";
+
+const daysBetweenDates = (dateA, dateB) => {
+  const firstMoment = moment(dateA);
+  const secondMoment = moment(dateB);
 
   return firstMoment.diff(secondMoment, "days");
 };
 
-const TableRow = props => {
-  const [color, setColor] = useState();
+const calculateBookingLength = booking => {
+  return daysBetweenDates(booking.checkOutDate, booking.checkInDate);
+};
 
-  const highlight = () => {
-    setColor("pink");
+const TableRow = props => (
+  <tr
+    className={props.isSelected ? "selected-search-row" : undefined}
+    onClick={props.handleClick}
+  >
+    <th scope="row">{props.booking.id}</th>
+    <td>{props.booking.title}</td>
+    <td>{props.booking.firstName}</td>
+    <td>{props.booking.surname}</td>
+    <td>{props.booking.email}</td>
+    <td>{props.booking.roomId}</td>
+    <td>{props.booking.checkInDate}</td>
+    <td>{props.booking.checkOutDate}</td>
+    <td>{calculateBookingLength(props.booking)}</td>
+    <td>
+      <button onClick={() => props.setShowProfile(props.booking.id)}>
+        Show Profile
+      </button>
+    </td>
+  </tr>
+);
 
-    if (color === "pink") {
-      setColor("none");
+const SearchResults = props => {
+  const [selectedRows, setSelectedRows] = useState([]);
+  // removes or adds an index to the selectedRows state array variable
+  const toggleSelectedAtPosition = index => {
+    // check if the given index is in the selectedRows array
+    if (selectedRows.includes(index)) {
+      // if it is:
+      // create a new array without this index
+      const newArray = selectedRows.filter(i => i !== index);
+      // setSelectedRows with this new array
+      setSelectedRows(newArray);
+    } else {
+      // if it's NOT:
+      //create a new array which incldues this index
+      const newArray = selectedRows.concat(index);
+      // setSelectedRows with this new array
+      setSelectedRows(newArray);
     }
   };
 
   return (
-    <tr className={color} onClick={highlight}>
-      <td scope="col">{props.booking.id}</td>
-      <td scope="col">{props.booking.title}</td>
-      <td scope="col">{props.booking.firstName}</td>
-      <td scope="col">{props.booking.surname}</td>
-      <td scope="col">{props.booking.email}</td>
-      <td scope="col">{props.booking.roomId}</td>
-      <td scope="col">{props.booking.checkInDate}</td>
-      <td scope="col">{props.booking.checkOutDate}</td>
-      <td scope="col">
-        {daysBetweenDates(
-          props.booking.checkOutDate,
-          props.booking.checkInDate
-        )}
-      </td>
-    </tr>
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Title</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Surname</th>
+          <th scope="col">Email</th>
+          <th scope="col">Room Id</th>
+          <th scope="col">Check-in</th>
+          <th scope="col">Check-out</th>
+          <th scope="col">Number of Nights</th>
+          <th scope="col" />
+        </tr>
+      </thead>
+      <tbody>
+        {props.results.map((booking, i) => (
+          <TableRow
+            key={i}
+            booking={booking}
+            setShowProfile={val => props.onShowCustomerProfile(val)}
+            handleClick={() => toggleSelectedAtPosition(i)}
+            isSelected={selectedRows.includes(i)}
+          />
+        ))}
+      </tbody>
+    </table>
   );
 };
-
-const SearchResults = props => (
-  <table className="table">
-    <thead>
-      <tr>
-        <th scope="col">Id</th>
-        <th scope="col">Title</th>
-        <th scope="col">First Name</th>
-        <th scope="col">Surname</th>
-        <th scope="col">Email</th>
-        <th scope="col">Room Id</th>
-        <th scope="col">Check in Date</th>
-        <th scope="col">Check Out Date</th>
-        <th scope="col">Nights</th>
-      </tr>
-    </thead>
-    <tbody>
-      {props.results.map((booking, index) => (
-        <TableRow key={index} booking={booking} />
-      ))}
-    </tbody>
-  </table>
-);
 
 export default SearchResults;
